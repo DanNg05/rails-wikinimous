ff9f946ccde157235d1180ba4d4b3cf8e2d7f09f
Dan Nguyen <sydan05071997@gmail.com>
2024-03-16 14:56:53 +1030

    challenge 3

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
@@ -8,22 +8,34 @@ class ArticlesController < ApplicationController
 
   end
 
-  def create
+  def new
+    @article = Article.new
   end
 
-  def new
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
   end
 
   def edit
   end
 
   def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path, status: :see_other
   end
 
   private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 
   def set_article
     @article = Article.find(params[:id])
